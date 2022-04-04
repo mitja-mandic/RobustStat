a69f79a30138bea12603c23423930a6579fcf910
mitja-mandic <mitja.mandic@gmail.com>
2022-04-04 19:58:53 +0200

    legends

--- robustStat_project.R
+++ robustStat_project.R
@@ -2,18 +2,10 @@ library(robustbase)
 library(sn)
 library(MASS)
 library(mrfDepth)
-<<<<<<< HEAD
-library(CensMFM)
-#set.seed(30)
-contamination = 0.1
-p = 10
-n = 1000
-=======
 set.seed(30)
 contamination <- 0.1
 p <- 5
 n <- 200
->>>>>>> b53ad00b83b4d503fc11ac1d5d6e0fe8ee2f8623
 
 alpha.2 <- c(10, 4)
 alpha.5 <- c(10, 10, 4, 4, 4)
@@ -26,54 +18,6 @@ incorrect.SD <- rep(0, length(distancesOutliers))
 incorrect.AO <- rep(0, length(distancesOutliers))
 
 omega <- diag(p)
-<<<<<<< HEAD
-#Function from package sn to generate skewed normal distribution. Added column of 1 to distinguish from 
-#added noise later. When changing p, don't forget to change alpha!!
-nRegular <- n*(1-contamination)
-generated <- rmsn(n=nRegular, xi=rep(0,length(alpha.10)), Omega=omega, alpha = alpha.10)
-generated <- cbind(generated,rep(0,nRegular))
-
-# generated.alt <- rMSN(n=nRegular,mu=rep(0,length(alpha.10)), Sigma = omega, shape=alpha.10)
-# generated.alt <- cbind(generated.alt,rep(0,nRegular))
-
-nOut <- n*contamination
-#Multivariate normal outliers (10% of m)
-outliers <- mvrnorm(nOut, mu = rep(-1.3,length(alpha.10)), Sigma = omega/20)
-outliers <- cbind(outliers, rep(1,nOut))
-
-#added the mixed skewed normal with outliers and shuffled them (not sure if shuffling is necessary though,
-#just seemed nicer) 
-simulated <- rbind(outliers, generated)
-simulated <- simulated[sample(1:nrow(simulated)),]
-
-#Calculated adjusted outlyingness and necessary values for boxplot cutoff
-#IQR constant 0.7478975
-
-adjOutl <- adjOutlyingness(simulated[,1:p])
-AO.values <- adjOutl$adjout
-AO.q3 <- quantile(AO.values, 0.75)
-AO.mc <- mc(AO.values)
-AO.IQR <- IQR(AO.values)
-AO.cutoff <- AO.q3 + 1.5*exp(3*AO.mc)*AO.IQR
-
-#Searched for an implemented SD funciton - tried this, but got a lot larger values than from AO and not
-#sure why. From package mrfDepth
-
-stahelDonoho <- outlyingness(x=simulated[,1:p])
-SD.out.values <- stahelDonoho$outlyingnessX
-SD.q3 <- quantile(SD.out.values,0.75)
-SD.mc <- mc(SD.out.values)
-SD.IQR <- IQR(SD.out.values)
-SD.cutoff <- SD.q3 + 1.5*exp(3*SD.mc)*SD.IQR
-
-outlyingnessTable <- as.data.frame(cbind(AO.values,SD.out.values,simulated[,ncol(simulated)]))
-colnames(outlyingnessTable) <- c("AO","SD","outlier")
-
-outlyingnessTable["classified by AO"] <- AO.values > AO.cutoff
-outlyingnessTable["classified by SD"] <- SD.out.values > SD.cutoff
-
-
-=======
 # Function from package sn to generate skewed normal distribution. Added column of 1 to distinguish from
 # added noise later. When changing p, don't forget to change alpha!!
 nRegular <- n * (1 - contamination)
@@ -135,9 +79,11 @@ for (i in 1:length(distancesOutliers)) {
 }
 
 # generate correct plot
-plot(distancesOutliers, correct.AO, col="red")
-lines(distancesOutliers, correct.SD, col="green")
+plot(distancesOutliers, correct.AO,type="l", col="red")
+lines(distancesOutliers, correct.SD,type="l", col="green")
+legend("topleft",legend = c("SD","AO"),col = c("green","red"),lty=1)
+
 # generate incorrect plot
-plot(distancesOutliers, incorrect.AO, col="red")
+plot(distancesOutliers, incorrect.AO,type="l", col="red")
 lines(distancesOutliers, incorrect.SD, col="green")
->>>>>>> b53ad00b83b4d503fc11ac1d5d6e0fe8ee2f8623
+legend("topright",legend = c("SD","AO"),col = c("green","red"),lty=1)
